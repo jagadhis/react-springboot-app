518a2c85386da083d99b75259c48a93a5f6046c3
jagadhis <jwaran78@gmail.com>
2022-04-16 21:47:14 +0530

    done for today

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,6 +6,16 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>Hello World</h1>
+        <table className="table table-striped table-bordered">
+          <thead>
+            <tr>
+              <th> Employee First Name</th>
+              <th> Employee Last Name</th>
+              <th> Employee Email Id</th>
+              <th> Actions</th>
+            </tr>
+          </thead>
+        </table>
       </header>
     </div>
   );
