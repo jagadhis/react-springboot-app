cb9db67abafc4f0b8c3b94069a604ca51ac1a26c
jagadhis <jwaran78@gmail.com>
2022-04-24 10:39:57 +0530

    undefinedd issue

--- frontend/src/Components/Listcars.jsx
+++ frontend/src/Components/Listcars.jsx
@@ -1,24 +1,34 @@
 import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 
 const Listcars = () => {
+  const { id } = useParams();
   const [Cars, setcars] = useState([]);
 
   let history = useNavigate();
 
   const BASEURL = "http://localhost:8080/api/v1/cars";
+  const DELETEURL = `http://localhost:8080/api/v1/cars/${id}`;
   useEffect(() => {
     axios.get(BASEURL).then((res) => {
       setcars(res.data);
     });
   }, []);
+
   const addCars = () => {
     history("/Addcars");
   };
   const editcars = (id) => {
     history(`/Updatecars/${id}`);
   };
+
+  const deletecars = () => {
+    axios.delete(DELETEURL, id).then((res) => {
+      setcars({ Cars: Cars.filter((cars) => cars.id !== id) });
+    });
+  };
+  console.log(id);
   return (
     <div>
       <h2 className="text-center">Cars list</h2>
@@ -49,6 +59,13 @@ const Listcars = () => {
                     className="btn btn-info"
                   >
                     Update
+                  </button>
+                  <button
+                    style={{ marginLeft: "10px" }}
+                    onClick={() => deletecars(cars.id)}
+                    className="btn btn-danger"
+                  >
+                    Delete
                   </button>
                 </td>
               </tr>
