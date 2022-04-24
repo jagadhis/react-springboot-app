af52c5b8691ac549db5c23e303d623b325ff2acd
jagadhis <jwaran78@gmail.com>
2022-04-24 21:29:54 +0530

    Completed the Project except delete

--- frontend/src/Components/Viewcars.jsx
+++ frontend/src/Components/Viewcars.jsx
@@ -1,9 +1,36 @@
-import React from "react";
-
+import axios from "axios";
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
 const Viewcars = () => {
+  const { id } = useParams();
+  const [Cars, setcars] = useState([]);
+  const BASEURL = `http://localhost:8080/api/v1/cars/${id}`;
+
+  useEffect(() => {
+    axios.get(BASEURL).then((res) => {
+      setcars(res.data);
+    });
+  });
   return (
     <div>
-      <h2>View Cars</h2>
+      <br></br>
+      <div className="card col-md-6 offset-md-3 ">
+        <h3 className="text-center">View Cars Details</h3>
+        <div className="card-body ">
+          <div className="row">
+            <label>Car Name:</label>
+            <div>{Cars.carname}</div>
+          </div>
+          <div className="row">
+            <label>Car Number:</label>
+            <div>{Cars.carnum}</div>
+          </div>
+          <div className="row">
+            <label>Driver Name</label>
+            <div>{Cars.drivername}</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
